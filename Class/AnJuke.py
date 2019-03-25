b33b2498cc383cb823bdbd5dc61e90772cc300e3
sazlzndy <772466271@qq.com>
2019-03-25 18:08:21 +0800

    新增中环地产爬虫

--- Class/AnJuke.py
+++ Class/AnJuke.py
@@ -2,11 +2,13 @@ import requests
 from lxml import etree
 import pandas as pd #csv保存模块
 import time
+import pymysql
 import os
 from urllib.parse import urljoin
+from sqlalchemy import create_engine
 import random #随机模块
 
-
+#安居客
 class AnJuKe(object):
   def __init__(self):
     self._errorXpath =  "/html/body/div[2]/div" ; #安居客网页异常
@@ -105,7 +107,7 @@ class AnJuKe(object):
     return text
 
 
-
+#中环地产
 class ZhongHuan(object):
   def __init__(self):
     self._indexUrl = "http://nc.zhdclink.com"
@@ -113,6 +115,8 @@ class ZhongHuan(object):
     self._houseInfoXpath = "/html/body/div[5]/div[1]/ul/li" #房屋信息列表
     self._nextUrl = "/html/body/div[5]/div[2]/ul/a/li[@class='prev' and text()='下一页']" #下一页地址
     self._houseInfoCsvFilePaht = "Result/ZhongHuanInfos{0}.csv"  # url地址
+    self._urlModel = "http://nc.zhdclink.com/house/detail/{Id}" #url
+    self._proxtList = "https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list" #代理列表
 
   #下载房屋信息
   def DownHouses(self):
@@ -120,10 +124,17 @@ class ZhongHuan(object):
     url = self._baseUrl;
     while(len(url) > 0):
       url = self.SaveHouseInfo(data,url)
+      if len(data) > 1000:
+          self.SaveData(data, self._houseInfoCsvFilePaht)
+          data.clear()
     self.SaveData(data,self._houseInfoCsvFilePaht)
 
   def SaveHouseInfo(self,data,url):
-    infoHtml = etree.HTML(self.DownPage(url))
+    pageHtml = self.DownPage(url)
+    if len(pageHtml) == 0 :
+      print("未成功下载{0}数据，结束本次抓取。".format(url))
+      return ""
+    infoHtml = etree.HTML(pageHtml)
     infoHtmlXapth = infoHtml.xpath(self._houseInfoXpath)
     for childInfo in infoHtmlXapth:
       title = childInfo.xpath(".//div[1]/span/a")[0]
@@ -136,15 +147,16 @@ class ZhongHuan(object):
       #print("保存{0}".format(etree.tostring(details)))
       print("保存{0}".format(title.text.strip()))
       data.append({
-        "标题": title.text.strip(),
-        "链接":urljoin(self._indexUrl,title.attrib['href']),
-        "房型":details[1],
-        "面积":details[2],
-        "楼层":commAddress[2],
-        "建造年份":commAddress[3],
-        "具体地址":commAddress[0],
-        "总价":priceDet,
-        "单价":unitPrice
+        "HouseInfoId" : urljoin(self._indexUrl,title.attrib['href']).replace(self._urlModel.replace("{Id}",""),""),
+        "Title": title.text.strip(),
+        "URL":urljoin(self._indexUrl,title.attrib['href']),
+        "HouseType":details[1],
+        "Area":details[2],
+        "Floor":commAddress[2],
+        "YearOfBuild":commAddress[3],
+        "SpecificAddress":commAddress[0],
+        "TotalPrices":priceDet,
+        "TheUnitPrice":unitPrice.replace("元/㎡","")
       })
     infoNextUrl = infoHtml.xpath(self._nextUrl)
     #print(infoNextUrl[0].xpath("..")[0].attrib['href'])
@@ -156,8 +168,11 @@ class ZhongHuan(object):
 
   #保存数据至本地
   def SaveData(self,data,filePath):
+    #db = pymysql.connect("localhost", "root", "123456", "houseinfo")
+    engine = create_engine('mysql+pymysql://root:123456@localhost:3306/houseinfo')
     pdData = pd.DataFrame(data)
-    pdData.to_csv(filePath, encoding='utf_8_sig')
+    pdData.to_csv(filePath.replace("{0}",time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))), encoding='utf_8_sig')
+    pdData.to_sql("houseinfo",engine,if_exists="append", index= False)
 
   #下载地区URL列表
   def DownURL(self):
@@ -185,18 +200,38 @@ class ZhongHuan(object):
 
     self.SaveData(data,self._csvFilePaht)
 
+  def GetProxies(self):
+    proxies = {
+      "http": "http://10.10.1.10:3128",
+      "https": "http://10.10.1.10:1080",
+    }
+    return proxies
+
   #下载网页
-  def DownPage(self, urlPath):
+  def DownPage(self, urlPath,index=300,proxies = ''):
     theHeaders = {
       'Referer': 'http://nc.zhdclink.com/house/index/',
       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
       #'Cookie' : 'aQQ_ajkguid=2400BD1D-02AC-9802-EC9E-3D24FFB25706; wmda_uuid=86ee38557d0872ca0b8196bdd23a8df9; wmda_new_uuid=1; wmda_visited_projects=%3B6289197098934; 58tj_uuid=d569f468-a28e-44bc-a1f2-1e9150242e71; als=0; browse_comm_ids=1026858; _ga=GA1.2.1746640190.1544437827; ctid=41; ANJUKE_BUCKET=pc-home%3AErshou_Web_Home_Home-a; sessid=BB9E53DA-8AB9-FEE7-1D23-E0F73FFBDE09; lps=http%3A%2F%2Fnc.anjuke.com%2Fsale%2Fhonggutannanchang-jiulonghuxinqu%2Fp2%2F%7C; twe=2; _gid=GA1.2.2068275622.1550455754; wmda_session_id_6289197098934=1550481535107-7934208b-6c8d-5ecb; init_refer=https%253A%252F%252Fnc.anjuke.com%252Fsale%252F; new_uv=7; new_session=0; __xsptplusUT_8=1; _gat=1; __xsptplus8=8.8.1550481535.%232%7Csp0.baidu.com%7C%7C%7Canjueke%7C%23%23F7_9kDasR4JqhV5BruqhF7SUfW8-Hr5O%23'.format(time.time())
+     'Cookie': "city_token=%E5%8D%97%E6%98%8C; login_token=uSDWeKN57IikxJ2sFdT4BXVRhU1OcqLEZlGgCMp9jm0fzrab; UM_distinctid=169b2fbeb7b300-0f7cb459947171-424e0b28-1fa400-169b2fbeb7c13c; CNZZDATA1273460933=1237323342-1553485980-%7C1553492604"
     }
-    #time.sleep(0.1)
+    time.sleep(0.3)
     print("下载{}数据中".format(urlPath))
-    text = requests.get(url=urlPath,headers=theHeaders).text
+
+    text = requests.get(url=urlPath,headers=theHeaders,proxies=proxies).text
+    if text.find("502 Bad Gateway") >= 0 :
+        if index < 3600:
+            time.sleep(index)
+            index = index*2
+            print("重试{1}次下载{0}数据中{2}".format(urlPath,index,time.strftime('%H:%M:%S',time.localtime(time.time()))))
+            return self.DownPage(urlPath,index)
+        else:
+            return ""
+    else:
+        return text
     #print(text)
-    return text
+
+
 
 
 class HongJi(object):
